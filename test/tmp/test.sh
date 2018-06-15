b1034c8c7aab17d79273596e5c99eae331f0dec7
hkaalep <hkaalep@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2018-06-15 14:53:27 +0000

    cleanup net added to xfst commands for Xerox build rules in makefile; attempted to make x_declinations.lexc more readable by re-grouping cont. lexicons
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/experiment-langs/est@168147 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- test/tmp/test.sh
+++ test/tmp/test.sh
@@ -8,8 +8,8 @@ exit
 
 cat ~/svn-giellatekno/main/experiment-langs/est/test/tmp/korpustest-8-alakriips | sed 's/    .*$//' | ~/xfst/bin/lookup ../../src/analyser-gt-desc.xfst -q -flags TT > ~/svn-giellatekno/main/experiment-langs/est/test/tmp/korpustest-8-alakriips.hjk.xfst
 
-cat ../test/tmp/korpustest-8-alakriips.hjk.xfst | grep '?' | sed 's/\t.*$//' > ../test/tmp/eitunne.xfst
-
+cat korpustest-8-alakriips.hjk.xfst | grep '?' | sed 's/\t.*$//' > eitunne.xfst
+-----------------
 cat ~/svn-giellatekno/main/experiment-langs/est/test/tmp/korpustest-8-alakriips | sed 's/    .*$//' | hfst-lookup analyser-gt-desc.hfstol > ~/svn-giellatekno/main/experiment-langs/est/test/tmp/korpustest-8-alakriips.hjk.hfst
 
 cat ../test/tmp/korpustest-8-alakriips.hjk.hfst | grep '?' | sed 's/\t.*$//' > ../test/tmp/eitunne.hfst
